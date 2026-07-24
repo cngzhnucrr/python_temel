9777d98a1c89337e1e672043c69c29809d06ef0d
cngzhnucrr <cnzucr@gmail.com>
2026-07-24 16:19:39 +0300

    düzenleme yapıldı

--- ornek_cozum/or1_cozum.py
+++ ornek_cozum/or1_cozum.py
@@ -1,31 +1,23 @@
 """soru:
     genel terimi 
     an=5+9+13+17+21+....+(4n+1)
-    
     olan dizinin
     ilk üç terim toplamını bulunuz.
-    
-    """
-
-"""  çözüm 1  """
-
 """
-n=2
+"""  çözüm 1  """
+"""
+n=2 
 an=4*n+1
 top:int=0
-
 for i in range(n):
     an=4*(i+1)+1
     top=top+an
     if (i+1)==n:
         print(top)
-        break
-    
-    
+        break 
 """
 """ çözüm 2"""    
-
-top=0
+top:int=0
 n=int(input("ilk kaç terim toplamını istiyorsunuz: "))
 def a(n):
      t=n*4+1
@@ -33,7 +25,8 @@ def a(n):
 
 for i in range(1,n):
     top=top+a(i)
+    print(f"ilk {i} terim toplamı:{top}")
     
-print(f"ilk {n} terim toplamı: {top} sunucudur.")
+print(f"ilk {n} terim toplamı: {top} sonucudur.")
 
         
