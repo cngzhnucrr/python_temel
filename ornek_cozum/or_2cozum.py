3d93bccc8554acea3a3aaea62dda139a38c042fd
cngzhnucrr <cnzucr@gmail.com>
2026-07-26 22:09:37 +0300

    hata düzeltildi kod yapısı düzenlendi ve açıklamalar eklendi

--- ornek_cozum/or_2cozum.py
+++ ornek_cozum/or_2cozum.py
@@ -12,28 +12,22 @@
        Bu terimlerin toplamını ekrana yazdırın.
     
     """
-a1:int =7
-d:int=4
+a1:int =7                       #dizinin ilk terimini tanımlayıp ilk değer ataması yaptık
+d:int=4                         #ortak farkı integer türünde tanımlayıp ilk değer ataması yaptık
 
 
-def a(n):
-    gen_ter=a1+(n-1)*d
-    return gen_ter
+def a(n):                       #dizinin genel terimi için fonksiyon yapısı kurduk
+    gen_ter=a1+(n-1)*d          #genel terimin yapısını oluşturduk 
+    return gen_ter              #genel terimin ürettiği sonucu dışa döndürdük
 
-i:int=1 
-aritmetik_dizi=[]
-while len(aritmetik_dizi)<100:
+i:int=1                         #döngü için sayac ayarladık
+aritmetik_dizi=[]               #genel terimden üreteceğimiz her değeri kaydetmek için liste tanımladık
+while len(aritmetik_dizi)<100:  #ilk 100 eleman için döngü kuralı tanımladık
+    aritmetik_dizi.append(a(i)) #genel terimden üretilen her elemanı listeye ekledik
     i=i+1
-    aritmetik_dizi.append(a(i))
 
-hedef_terimler=[]
-
-a:int=0
-while a<100:
-   for t in aritmetik_dizi:
-        if t%3==0:
-            if t%5!=0:
-                print("python üçe tam bölüneni ve 5 e bölünemeyeni buldu",t)
-                hedef_terimler.append(t)
-                a=a+1
-                
+hedef_terimler=[]               #kuralın geçerli olduğu elemanları eklemek için yeni bir liste tanımladık
+for t in aritmetik_dizi:        #kuralı uygulamak için önceki listede oluştutulan elemanları listeyi tarayacak şekilde döngüye aldık
+    if t%3==0:                  #3ile tam bölünebilen elemanlar 
+        if t%5!=0:              #5 ile tam bölünemeyen elemanlar
+            hedef_terimler.append(t) #kuralı sağlayan elemanları listeye ekle dedik
